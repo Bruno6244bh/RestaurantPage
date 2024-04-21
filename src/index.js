110fe7dbf347816827bd832094fef85fd25c6c65
Bruno <bruno6244.bh@gmail.com>
2024-04-21 03:43:01 -0300

    Complete page

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import '../public/index.css';
+
+
+    document.querySelector('.button-openMenu').addEventListener('click', function() {
+        window.open("../media/Menu - La Cuisine.pdf", '_blank');
+    });
